a1cffd66f16c561b5047e8469e94a342033157e0
AliseBeth <elizaveta.05031996@yandex.ru>
2023-11-08 12:54:33 +0300

    feat: add changing welcome slides on time

--- for-site/script.js
+++ for-site/script.js
@@ -14,3 +14,34 @@ for (let i = 0; i < welcome_btn.length; i++) {
         welcome_btn[i].classList.add('black');
     }
 }
+
+
+var currentSlide = 0;
+
+
+setInterval(function() {
+
+    for (let b = 0; b < welcome_btn.length; b++) {
+        welcome_window[b].classList.add('hidden');
+        welcome_btn[b].classList.remove('black');
+    };
+
+    for (let n = 0; n < welcome_btn.length; n++) {
+        welcome_btn[n].addEventListener("click", changeCurrent);
+        
+        function changeCurrent(event) {
+            currentSlide = n;
+        }
+    };
+  
+    welcome_window[currentSlide].classList.remove('hidden');
+    welcome_btn[currentSlide].classList.add('black');
+    
+    currentSlide++;
+
+    if (currentSlide >= welcome_window.length) {
+        currentSlide = 0;
+    }
+    
+
+}, 2000); 
