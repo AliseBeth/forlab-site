e36bc1602e27ecc8cbdf7769165ecf3e0ef88803
AliseBeth <elizaveta.05031996@yandex.ru>
2023-11-27 14:54:56 +0300

    feat: done left slider on sport page

--- for-site/script.js
+++ for-site/script.js
@@ -106,7 +106,24 @@ for (let i = 0; i < sp_pic_container.length; i++) {
     sp_pics_right[i].addEventListener("click", clickRight);
     let a = 0; 
     function clickLeft(event) {
-        console.log(i)
+        console.log(i + 1, "номер окна")
+        
+
+        sp_pic[a + (i * 3)].classList.add('hidden');
+
+        console.log(a, "номер картинки перед иф")
+
+        if (a <= 0) {
+            a = 2;
+            sp_pic[a + (i * 3)].classList.remove('hidden');
+        } else {
+            sp_pic[(a - 1) + (i * 3)].classList.remove('hidden');
+            a--
+            console.log("отработал элс")
+        }
+
+        console.log(sp_pic.length, "колво картинок")
+        console.log(a, "номер картинки")
     }
     function clickRight(event) {
 
