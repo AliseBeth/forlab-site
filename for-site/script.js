179f70b3c1c55b21a38a4e550edc700760bd9f48
AliseBeth <elizaveta.05031996@yandex.ru>
2023-11-27 16:09:06 +0300

    feat: add pictures to the main page slider

--- for-site/script.js
+++ for-site/script.js
@@ -105,10 +105,9 @@ for (let i = 0; i < sp_pic_container.length; i++) {
     sp_pics_left[i].addEventListener("click", clickLeft);
     sp_pics_right[i].addEventListener("click", clickRight);
     let a = 0; 
+
     function clickLeft(event) {
         console.log(i + 1, "номер окна")
-        
-
         sp_pic[a + (i * 3)].classList.add('hidden');
 
         console.log(a, "номер картинки перед иф")
@@ -125,11 +124,9 @@ for (let i = 0; i < sp_pic_container.length; i++) {
         console.log(sp_pic.length, "колво картинок")
         console.log(a, "номер картинки")
     }
+
     function clickRight(event) {
-
         console.log(i + 1, "номер окна")
-        
-
         sp_pic[a + (i * 3)].classList.add('hidden');
 
         console.log(a, "номер картинки перед иф")
@@ -144,8 +141,5 @@ for (let i = 0; i < sp_pic_container.length; i++) {
 
         console.log(sp_pic.length, "колво картинок")
         console.log(a, "номер картинки")
-    }
-        
-
-                        
+    }                    
 }
