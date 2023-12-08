38a60de68a40367b350645e866ae97206328b430
AliseBeth <elizaveta.05031996@yandex.ru>
2023-12-08 14:57:01 +0300

    feat: debug

--- for-site/main scripts/changing-headers.js
+++ for-site/main scripts/changing-headers.js
@@ -2,11 +2,9 @@ const headers = document.querySelectorAll('.welcome-changing-header');
 const point = document.querySelector('.welcome-point');
 
 
-
-
+//changing headers
 
 let actual_header = 0;
-
 setInterval(function() {
     
     if (actual_header >= headers.length) {
@@ -25,6 +23,10 @@ setInterval(function() {
 }, 1000); 
 
 
+
+
+// blinking point
+
 let i = 1;
 setInterval(function() {
     
@@ -35,6 +37,8 @@ setInterval(function() {
         point.classList.remove('hidden');
         i++
     }
-
+    if (i === 3) {
+        i = 1;
+    }
 }, 400); 
     
